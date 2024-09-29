10ba5d3b4d71ee2946ec9ca2f765caaf715651af
ArneJohnson <arnejohnson02@gmail.com>
2024-09-29 20:17:19 +0200

    loading screen + motion detected painting

--- script.js
+++ script.js
@@ -1,60 +1,52 @@
 let handpose;
 let video;
-let pg;
 let hands = [];
+let loading;
 
 function preload() {
   handpose = ml5.handPose();
+  loading = true;
 }
 
 function setup() {
   createCanvas(innerWidth, innerHeight);
+  frameRate(60);
   background(255);
 
   video = createCapture(VIDEO);
   video.size(innerWidth, innerHeight);
   video.hide();
 
-  frameRate(60);
-
   handpose.detectStart(video, getHandsData);
+
+  setTimeout(() => {
+    loading = false;
+  }, 7000);
 }
 
 function draw() {
   // Flips the camera and draws a photo of it
   translate(innerWidth, 0);
   scale(-1, 1);
-  //image(video, -resolution, 0, resolution, resolution);
+  //-----For testing with camera----- 
+  //image(video, 0, 0, innerWidth, innerHeight);
 
-  // Clear and create an overlay on the canvas.
-  // Creates a circle between the size of the distance between the index finger and thumb
-  // From Bassima Example
-  // for (let hand of hands) {
-  //   let indexFinger = hand.index_finger_tip;
-  //   let thumb = hand.thumb_tip;
-
-  //   let centerX = (indexFinger.x + thumb.x) / 2;
-  //   let centerY = (indexFinger.y + thumb.y) / 2;
-
-  //   let distance = dist(indexFinger.x, indexFinger.y, thumb.x, thumb.y);
-
-  //   stroke(0);
-  //   strokeWeight(2);
-  //   noFill();
-  //   ellipse(centerX, centerY, distance);
-  // }
-
-  //////////////////CODE FOR DRAWING ELLIPSE ON THE INDEX FINGER AND THUMB//////////////////
-  // From Bassima example
   if (hands.length > 0) {
-    let indexFinger = hands[0].index_finger_tip;
+    let middle = hands[0].middle_finger_tip;
+    let index = hands[0].index_finger_tip;
     let thumb = hands[0].thumb_tip;
 
-    fill(0, 255, 0);
-    ellipse(indexFinger.x, indexFinger.y, 10);
-    ellipse(thumb.x, thumb.y, 10);
+    if (dist(middle.x, middle.y, thumb.x, thumb.y) < 70) {
+      let centerX = (index.x + thumb.x) / 2;
+      let centerY = (index.y + thumb.y) / 2;
+
+      let distance = dist(index.x, index.y, thumb.x, thumb.y);
+
+      noStroke();
+      fill("blue");
+      ellipse(centerX, centerY, distance);
+    }
   }
-  //////////////////CODE FOR DRAWING ELLIPSE ON THE INDEX FINGER AND THUMB//////////////////
 }
 
 function getHandsData(results) {
@@ -76,18 +68,30 @@ let overlay = function (overlay) {
     overlay.scale(-1, 1);
 
     for (let hand of hands) {
-      let indexFinger = hand.index_finger_tip;
+      let index = hand.index_finger_tip;
       let thumb = hand.thumb_tip;
 
-      let centerX = (indexFinger.x + thumb.x) / 2;
-      let centerY = (indexFinger.y + thumb.y) / 2;
+      let centerX = (index.x + thumb.x) / 2;
+      let centerY = (index.y + thumb.y) / 2;
 
-      let distance = dist(indexFinger.x, indexFinger.y, thumb.x, thumb.y);
+      let distance = dist(index.x, index.y, thumb.x, thumb.y);
 
       overlay.stroke(0);
       overlay.strokeWeight(2);
       overlay.noFill();
       overlay.ellipse(centerX, centerY, distance);
+    }
+
+    if (loading) {
+      overlay.push();
+      overlay.translate(innerWidth, 0);
+      overlay.scale(-1, 1);
+      overlay.fill(0);
+      overlay.rect(0, 0, innerWidth, innerHeight);
+      overlay.textSize(48);
+      overlay.fill(255);
+      overlay.text("loading...", 100, 100);
+      overlay.pop();
     }
   }
 };
