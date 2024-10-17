8a6f2c56582b9410f5fb64ebe311217b2cb9ab13
ArneJohnson <arnejohnson02@gmail.com>
2024-10-17 11:27:36 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import * as handpose from "@tensorflow-models/handpose";
 import * as faceapi from "face-api.js";
 import Webcam from "react-webcam";
 import "./App.css";
-//import { drawHand } from "./utilities";
 import Sketch from "react-p5";
 import * as brush from "p5.brush";
 
@@ -26,22 +25,15 @@ function App() {
   const detections = useRef(null);
   const [handsloaded, setHandsloaded] = useState(false);
   const [color, setColor] = useState("ffba59");
+  const [emotion, setEmotion] = useState("Neutral");
+  const [sizeX, setSizeX] = useState(100);
+  const [sizeY, setSizeY] = useState(100);
 
   const [faceDetections, setFaceDetections] = useState([]);
 
   useEffect(() => {
     runDetection();
-    //setupDrawingCanvas();
   }, []);
-
-  // const setupDrawingCanvas = () => {
-  //   const drawingCanvas = drawingCanvasRef.current;
-  //   const ctx = drawingCanvas.getContext("2d");
-  //   ctx.strokeStyle = "blue"; // Default stroke color
-  //   ctx.lineWidth = 4;
-  //   ctx.lineCap = "round";
-  //   ctx.lineJoin = "round"; // Smooth line joins
-  // };
 
   const runDetection = async () => {
     await faceapi.nets.tinyFaceDetector.loadFromUri("/models");
@@ -54,8 +46,8 @@ function App() {
   const calculate3DDistance = (point1, point2) => {
     return Math.sqrt(
       Math.pow(point1[0] - point2[0], 2) +
-        Math.pow(point1[1] - point2[1], 2) +
-        Math.pow(point1[2] - point2[2], 2)
+      Math.pow(point1[1] - point2[1], 2) +
+      Math.pow(point1[2] - point2[2], 2)
     );
   };
 
@@ -78,42 +70,10 @@ function App() {
     return avg;
   };
 
-  // const drawLine = (ctx, x1, y1, x2, y2) => {
-  //   ctx.beginPath();
-  //   ctx.moveTo(x1, y1);
-  //   ctx.lineTo(x2, y2);
-  //   ctx.stroke();
-
-  //   drawingHistory.current.push({
-  //     x1,
-  //     y1,
-  //     x2,
-  //     y2,
-  //     style: ctx.strokeStyle,
-  //     width: ctx.lineWidth,
-  //   });
-  // };
-
-  // const redrawHistory = () => {
-  //   const ctx = drawingCanvasRef.current.getContext("2d");
-  //   ctx.clearRect(
-  //     0,
-  //     0,
-  //     drawingCanvasRef.current.width,
-  //     drawingCanvasRef.current.height
-  //   );
-
-  //   drawingHistory.current.forEach((line) => {
-  //     ctx.strokeStyle = line.style;
-  //     ctx.lineWidth = line.width;
-  //     drawLine(ctx, line.x1, line.y1, line.x2, line.y2);
-  //   });
-  // };
-
   const calculateFingerDistance = (finger1, finger2) => {
     return Math.sqrt(
       Math.pow(finger1[0] - finger2[0], 2) +
-        Math.pow(finger1[1] - finger2[1], 2)
+      Math.pow(finger1[1] - finger2[1], 2)
     );
   };
 
@@ -135,9 +95,9 @@ function App() {
     p5.frameRate(60);
 
     const saveBtn = p5.createButton("Save Canvas");
-    saveBtn.position(20, 20);
+    saveBtn.position(120, 75);
     saveBtn.mousePressed(() => {
-      p5.saveCanvas("canvas", "png");
+      p5.saveCanvas('canvas', 'png');
     });
 
     brush.instance(p5);
@@ -148,11 +108,12 @@ function App() {
 
     // Brushes for each emotion
     brush.add("happy", {
-      type: "custom",
+      type: "spray",
       weight: 5,
       vibration: 0.08,
-      opacity: 23,
-      spacing: 0.6,
+      definition: 0.5,
+      opacity: 50,
+      spacing: 3,
       blend: true,
       pressure: {
         type: "standard",
@@ -180,7 +141,7 @@ function App() {
     brush.add("angry", {
       type: "custom",
       weight: 10,
-      vibration: 0.5,
+      vibration: 5,
       opacity: 28,
       spacing: 1,
       blend: true,
@@ -373,9 +334,8 @@ function App() {
     });
   };
 
-  let emotionTriggered = false;
   let lastTriggerTime = 0;
-  const triggerCooldown = 5000;
+  const triggerTime = 5000;
 
   let lastAnxietyTime = 0;
   const anxietyCooldown = 3000;
@@ -408,6 +368,10 @@ function App() {
   const greedCooldown = 3000;
 
   const draw = async (p5) => {
+    if (p5.mouseIsPressed) {
+      p5.saveCanvas('canvas', 'png');
+    }
+
     if (webcamRef.current && webcamRef.current.video.readyState === 4) {
       const video = webcamRef.current.video;
 
@@ -418,7 +382,11 @@ function App() {
 
       setFaceDetections(detections.current);
 
-      if (detections.current.length > 0 && !emotionTriggered) {
+      if (
+        detections.current.length > 0 &&
+        Date.now() - lastTriggerTime > triggerTime &&
+        p5.frameCount % 6 === 0
+      ) {
         const expressions = detections.current[0].expressions;
         const availableEmotions = [
           expressions.happy,
@@ -432,22 +400,47 @@ function App() {
           case 0:
             brush.pick("happy");
             setColor("#ffde59");
+            setEmotion("Happy");
+            setSizeX(p5.random(150, 200));
+            setSizeY(100);
+            brush.bleed(p5.random(0.3, 0.5), "out");
+            brush.fillTexture(0.3, 0.1);
             break;
           case 1:
             brush.pick("sad");
             setColor("#38b6ff");
+            setEmotion("Sad");
+            setSizeX(100);
+            setSizeY(p5.random(150, 200));
+            brush.bleed(p5.random(0.3, 0.5), "out");
+            brush.fillTexture(0.2, 1);
             break;
           case 2:
             brush.pick("angry");
             setColor("#ff1717");
+            setEmotion("Angry");
+            setSizeX(p5.random(150, 200));
+            setSizeY(p5.random(150, 200));
+            brush.bleed(p5.random(0.05, 0.2), "out");
+            brush.fillTexture(0.4, 0.8);
             break;
           case 3:
             brush.pick("disgust");
             setColor("#c9e165");
+            setEmotion("Disgusted");
+            setSizeX(100);
+            setSizeY(p5.random(120, 150));
+            brush.bleed(p5.random(2, 0.4), "in");
+            brush.fillTexture(0.55, 0.5);
             break;
           case 4:
             brush.pick("fear");
             setColor("#c9e165");
+            setEmotion("Fearful");
+            setSizeX(p5.random(50, 120));
+            setSizeY(70);
+            brush.bleed(p5.random(0.4, 0.5), "in");
+            brush.fillTexture(0.55, p5.random(0.8, 1));
             break;
           default:
             break;
@@ -455,99 +448,161 @@ function App() {
       }
 
       // Randomly trigger the anxiety brush
-      if (
-        Math.random() < 0.3 &&
+      else if (
+        Math.random() < 0.02 &&
         Date.now() - lastAnxietyTime > anxietyCooldown
       ) {
-        // 30% chance of system having an anxiety attack
         brush.pick("anxiety");
         setColor("#f67122");
+        setEmotion("Anxious");
+        setSizeX(100);
+        setSizeY(100);
+        brush.bleed(p5.random(0.4, 0.5), "in");
+        brush.fillTexture(0.55, p5.random(0.8, 1));
         lastAnxietyTime = Date.now();
         lastTriggerTime = Date.now();
       }
 
       // Randomly trigger envy
-      if (Math.random() < 0.02 && Date.now() - lastEnvyTime > envyCooldown) {
+      else if (
+        Math.random() < 0.02 &&
+        Date.now() - lastEnvyTime > envyCooldown
+      ) {
+        brush.pick("envy");
+        setColor("#8ead90");
+        setEmotion("Envious");
+        setSizeX(p5.random(150, 200));
+        setSizeY(p5.random(150, 200));
+        brush.bleed(p5.random(0.2, 0.5), "in");
+        brush.fillTexture(0.55, 0.5);
         lastEnvyTime = Date.now();
         lastTriggerTime = Date.now();
       }
 
       // Randomly trigger embarassment
-      if (
+      else if (
         Math.random() < 0.02 &&
         Date.now() - lastEmbarassmentTime > embarassmentCooldown
       ) {
         brush.pick("embarassment");
         setColor("#f85ebe");
+        setEmotion("Embarassed");
+        setSizeX(p5.random(50, 100));
+        setSizeY(p5.random(50, 100));
+        brush.bleed(0.4, "out");
+        brush.fillTexture(0.55, 0.5);
         lastEmbarassmentTime = Date.now();
         lastTriggerTime = Date.now();
       }
 
       // Randomly trigger boredom
-      if (
+      else if (
         Math.random() < 0.02 &&
         Date.now() - lastBoredomTime > boredomCooldown
       ) {
+        brush.pick("boredom");
         setColor("#5e69b9");
+        setEmotion("Bored");
+        setSizeX(100);
+        setSizeY(100);
+        brush.bleed(0.05);
+        brush.fillTexture(0.55, 1);
         lastBoredomTime = Date.now();
         lastTriggerTime = Date.now();
       }
 
       // Randomly trigger nostalgia
-      if (
+      else if (
         Math.random() < 0.02 &&
         Date.now() - lastNostalgiaTime > nostalgiaCooldown
       ) {
         brush.pick("nostalgia");
         setColor("#ae8175");
+        setEmotion("Nostalgic");
+        setSizeX(p5.random(80, 120));
+        setSizeY(p5.random(80, 120));
+        brush.bleed(p5.random(0.2, 0.4));
+        brush.fillTexture(0.1, 0.5);
         lastNostalgiaTime = Date.now();
         lastTriggerTime = Date.now();
       }
 
       // Randomly trigger sceptisism
-      if (
+      else if (
         Math.random() < 0.02 &&
         Date.now() - lastSceptisismTime > sceptisismCooldown
       ) {
         brush.pick("sceptisism");
         setColor("#7f832e");
+        setEmotion("Sceptic");
+        setSizeX(90);
+        setSizeY(p5.random(120, 150));
+        brush.bleed(p5.random(0.05, 0.3));
+        brush.fillTexture(0.55, 0.8);
         lastSceptisismTime = Date.now();
         lastTriggerTime = Date.now();
       }
 
       // Randomly trigger jealousy
-      if (
+      else if (
         Math.random() < 0.02 &&
         Date.now() - lastJealousyTime > jealousyCooldown
       ) {
         brush.pick("jealousy");
         setColor("#a5cd98");
+        setEmotion("Jealous");
+        setSizeX(p5.random(100, 160));
+        setSizeY(p5.random(100, 160));
+        brush.bleed(0.4, "in");
+        brush.fillTexture(0.55, 0.5);
         lastJealousyTime = Date.now();
         lastTriggerTime = Date.now();
       }
 
       // Randomly trigger schadenfreude
-      if (
+      else if (
         Math.random() < 0.02 &&
         Date.now() - lastSchadenfreudeTime > schadenfreudeCooldown
       ) {
+        brush.pick("schadenfreude");
         setColor("#a5cd98");
+        setEmotion("Schadenfreude");
+        setSizeX(p5.random(100, 120));
+        setSizeY(p5.random(100, 130));
+        brush.bleed(p5.random(0.05, 0.4), "out");
+        brush.fillTexture(0.55, 1);
         lastSchadenfreudeTime = Date.now();
         lastTriggerTime = Date.now();
       }
 
       // Randomly trigger shame
-      if (Math.random() < 0.02 && Date.now() - lastShameTime > shameCooldown) {
+      else if (
+        Math.random() < 0.02 &&
+        Date.now() - lastShameTime > shameCooldown
+      ) {
         brush.pick("shame");
         setColor("#6c959f");
+        setEmotion("Shameful");
+        setSizeX(p5.random(100, 120));
+        setSizeY(p5.random(50, 80));
+        brush.bleed(p5.random(0.3, 0.4));
+        brush.fillTexture(0.55, 0.2);
         lastShameTime = Date.now();
         lastTriggerTime = Date.now();
       }
 
       // Randomly trigger greed
-      if (Math.random() < 0.02 && Date.now() - lastGreedTime > greedCooldown) {
+      else if (
+        Math.random() < 0.02 &&
+        Date.now() - lastGreedTime > greedCooldown
+      ) {
         brush.pick("greed");
         setColor("#29c784");
+        setEmotion("Greedy");
+        setSizeX(p5.random(100, 160));
+        setSizeY(p5.random(100, 160));
+        brush.bleed(p5.random(0.05, 0.2));
+        brush.fillTexture(0.55, 0.9);
         lastGreedTime = Date.now();
         lastTriggerTime = Date.now();
       }
@@ -570,8 +625,8 @@ function App() {
             indexTip,
             middleTip
           );
-
-          if (thumbIndexDistance < 50 && indexMiddleDistance > 50) {
+          // if (thumbIndexDistance < 70 && indexMiddleDistance > 70) {
+          if (thumbIndexDistance < 50) {
             const currentPos = {
               x: p5.map(indexTip[0], 0, video.videoWidth, p5.width, 0),
               y: p5.map(indexTip[1], 0, video.videoHeight, 0, p5.height),
@@ -582,14 +637,12 @@ function App() {
             brush.noHatch();
             brush.noField();
             brush.noStroke();
-            brush.bleed(p5.random(0.05, 0.4));
-            brush.fillTexture(0.55, 0.5);
             brush.fill(color, p5.random(80, 140));
             brush.rect(
               smoothedPos.x - p5.width / 2,
               smoothedPos.y - p5.height / 2,
-              100,
-              100
+              sizeX,
+              sizeY
             );
           }
         }
@@ -597,7 +650,7 @@ function App() {
     }
   };
 
-  // Overlay to know where you are painting
+  // Overlay to know where you are painting and your current emotion
   const overlaySetup = (p5, canvasParentRef) => {
     p5.createCanvas(p5.windowWidth * 0.9, p5.windowHeight * 0.9).parent(
       canvasParentRef
@@ -618,6 +671,16 @@ function App() {
       const video = webcamRef.current.video;
 
       const hands = await handposeNet.current.estimateHands(video);
+
+      p5.textAlign(p5.RIGHT);
+      p5.textSize(28);
+      p5.noStroke();
+      p5.fill(0);
+      p5.text("Currently feeling", p5.width - 50, 50);
+      p5.stroke(0);
+      p5.strokeWeight(1);
+      p5.fill(color);
+      p5.text(emotion, p5.width - 50, 75);
 
       if (hands.length > 0) {
         const hand = hands[0];
